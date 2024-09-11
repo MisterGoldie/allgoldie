f0f66d931fab783e030e2a3a165bca247dc03076
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-09-10 21:39:30 -0500

    nsnnehr

--- api/index.tsx
+++ api/index.tsx
@@ -103,7 +103,7 @@ app.frame('/', (c) => {
     image: BACKGROUND_IMAGE,
     imageAspectRatio: '1.91:1',
     intents: [
-      <Button action="/check">Check Scary Garys</Button>
+      <Button action="/check">Check scary garys</Button>
     ],
   })
 })
